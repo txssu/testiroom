ceec394683b9e2455c61d0906292451792e4c7d9
Eugene <git@telegrafo.ru>
2023-11-10 21:02:44 +0500

    Use many_to_many assoc for selected options

--- lib/testiroom/exams/student_answer.ex
+++ lib/testiroom/exams/student_answer.ex
@@ -2,7 +2,7 @@ defmodule Testiroom.Exams.StudentAnswer do
   use Ecto.Schema
 
   alias Testiroom.Exams.Task
-  alias Testiroom.Exams.StudentAnswer.SelectedOption
+  alias Testiroom.Exams.Task.Option
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -10,7 +10,10 @@ defmodule Testiroom.Exams.StudentAnswer do
     field :text, :string
 
     belongs_to :task, Task
-    has_many :selected_options, SelectedOption, on_delete: :delete_all
+
+    many_to_many :selected_options, Option,
+      join_through: "test_task_student_answer_selected_options",
+      on_delete: :delete_all
 
     timestamps(type: :utc_datetime)
   end
--- priv/repo/migrations/20231110151806_create_test_task_student_answer_selected_options.exs
+++ priv/repo/migrations/20231110151806_create_test_task_student_answer_selected_options.exs
@@ -3,20 +3,16 @@ defmodule Testiroom.Repo.Migrations.CreateTestTaskStudentAnswerSelectedOptions d
 
   def change do
     create table(:test_task_student_answer_selected_options, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-
       add :student_answer_id,
           references(:test_task_students_answers, on_delete: :delete_all, type: :binary_id),
           null: false
 
-      add :task_option_id,
+      add :option_id,
           references(:test_task_options, on_delete: :delete_all, type: :binary_id),
           null: false
-
-      timestamps(type: :utc_datetime)
     end
 
     create index(:test_task_student_answer_selected_options, [:student_answer_id])
-    create index(:test_task_student_answer_selected_options, [:task_option_id])
+    create index(:test_task_student_answer_selected_options, [:option_id])
   end
 end
