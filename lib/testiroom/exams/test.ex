a5dab2305b73fe6b8a3a03723e6cf55d93243d16
Eugene <git@telegrafo.ru>
2023-11-10 20:29:54 +0500

    Add `has_many` tasks to `Test`

--- lib/testiroom/exams/test.ex
+++ lib/testiroom/exams/test.ex
@@ -1,10 +1,14 @@
 defmodule Testiroom.Exams.Test do
   use Ecto.Schema
 
+  alias Testiroom.Exams.Task
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "tests" do
     field :title, :string
+
+    has_many :tasks, Task
 
     timestamps(type: :utc_datetime)
   end
