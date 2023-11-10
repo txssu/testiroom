3a295686d213cc252d45376c757d663b44b75583
Eugene <git@telegrafo.ru>
2023-11-10 18:26:46 +0500

    Revert "Don't update changeset in answer form"
    This reverts commit 070be7a4108a0bb7040b7f9e5dbcb3b656099990.

--- lib/testiroom_web/live/exam/answer_form.ex
+++ lib/testiroom_web/live/exam/answer_form.ex
@@ -99,7 +99,7 @@ defmodule TestiroomWeb.AnswerForm do
 
     send(self(), {__MODULE__, Ecto.Changeset.apply_changes(changeset)})
 
-    {:noreply, socket}
+    {:noreply, assign_changeset(socket, changeset)}
   end
 
   def handle_event("ignore", _params, socket) do
