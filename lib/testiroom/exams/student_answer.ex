6b9a1d7facc2dcc4c2e25bc1e14b8ba0503e1e2f
Eugene <git@telegrafo.ru>
2023-11-10 20:43:05 +0500

    Add `selected_options` assoc to `StudentAnswer`

--- lib/testiroom/exams/student_answer.ex
+++ lib/testiroom/exams/student_answer.ex
@@ -2,6 +2,7 @@ defmodule Testiroom.Exams.StudentAnswer do
   use Ecto.Schema
 
   alias Testiroom.Exams.Task
+  alias Testiroom.Exams.StudentAnswer.SelectedOption
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -9,6 +10,7 @@ defmodule Testiroom.Exams.StudentAnswer do
     field :text, :string
 
     belongs_to :task, Task
+    has_many :selected_options, SelectedOption, on_delete: :delete_all
 
     timestamps(type: :utc_datetime)
   end
