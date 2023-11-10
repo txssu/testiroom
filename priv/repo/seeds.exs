b49b64a870f1e85f76033d3689cc96552a232636
Eugene <git@telegrafo.ru>
2023-11-10 18:26:47 +0500

    Revert "Add tests schemes"
    This reverts commit 5c46179f6f0320ca0ff6a9be27cfa8eeb430ffef.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,32 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-
-alias Testiroom.Exams.Test
-alias Testiroom.Exams.Task
-
-test = Testiroom.Repo.insert!(%Test{title: "Контрольная работа по математике №1"})
-
-Testiroom.Repo.insert!(%Task{
-  type: :shortanswer,
-  question: "Сколько будет 2+2?",
-  order: 1,
-  answer: "4",
-  test_id: test.id
-})
-
-Testiroom.Repo.insert!(%Task{
-  type: :shortanswer,
-  question: "Сколько будет 7+1?",
-  order: 2,
-  answer: "8",
-  test_id: test.id
-})
-
-Testiroom.Repo.insert!(%Task{
-  type: :shortanswer,
-  question: "Сколько будет 8-3?",
-  order: 3,
-  answer: "5",
-  test_id: test.id
-})
