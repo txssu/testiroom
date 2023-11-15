2cd2393a773abbb3be4bdb4d184955dd56152935
Eugene <git@telegrafo.ru>
2023-11-15 23:40:27 +0500

    Add redirect from mane page

--- lib/testiroom_web/controllers/page_controller.ex
+++ lib/testiroom_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule TestiroomWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+
+    redirect(conn, to: ~p"/tests")
   end
 end
