9de7aa09d641dac4c997eb766801a822bef1e9d7
Eugene <git@telegrafo.ru>
2023-11-20 20:56:58 +0500

    Use McCord way to dealing with nested schemas
    See [TodoTrek](https://github.com/chrismccord/todo_trek).
    
    Squashed commit of the following:
    
    commit cda15891b308aed8a52cb1c0df0cc5c112ada4ef
    Author: Eugene <git@telegrafo.ru>
    Date:   Mon Nov 20 20:53:54 2023 +0500
    
        Downcase first letter in error messages
    
    commit 893213331f923e249da2cd7a1b17ddb29fd5ee44
    Author: Eugene <git@telegrafo.ru>
    Date:   Mon Nov 6 13:32:08 2023 +0500
    
        Add translates to russian
    
    commit bac17bfe53c51a48b422e416aa70ceaeb3374bcd
    Author: Eugene <git@telegrafo.ru>
    Date:   Mon Nov 20 20:48:40 2023 +0500
    
        Fix test update
    
    commit ef40142c0cab5184b79698623f7b8cc00fcd01a9
    Author: Eugene <git@telegrafo.ru>
    Date:   Mon Nov 20 20:40:16 2023 +0500
    
        Fix nested deleting
    
    commit ba5f71a425ac30f14c4151c684fbf32df674a5cd
    Author: Eugene <git@telegrafo.ru>
    Date:   Mon Nov 20 20:36:35 2023 +0500
    
        Add correct_options_count validate
    
    commit 17fe54a33081d4e5f0a80f4e84367146bdf1e174
    Author: Eugene <git@telegrafo.ru>
    Date:   Mon Nov 20 20:06:47 2023 +0500
    
        Use `sort` and `drop` `_param`s for nested forms
    
    commit ad70fc3ac083a9bc3b6df3e0ef771d33e71fb3e8
    Merge: 12f02da c7d0100
    Author: Eugene <git@telegrafo.ru>
    Date:   Mon Nov 20 19:08:36 2023 +0500
    
        Merge branch 'dev' into add-test-creating
    
    commit 12f02da91c3d7f8957edf576543aa6f2299ef866
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 22:33:01 2023 +0500
    
        Add titles
    
    commit d1a6c79fc7434eb79268327e08e6b513694b0d11
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 22:26:59 2023 +0500
    
        Use proper navigation
    
    commit 466cd48920f4cd18401cf2d4e9c80e5a92984f13
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 20:11:35 2023 +0500
    
        Add styles to buttons
    
    commit 55eb6421388ed237f6e39c06137f7f0b42389e44
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 19:57:02 2023 +0500
    
        Add author to tests
    
    commit a4675504fe07cb8f3a7cb634a04db1c88b4bc5ca
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 19:37:54 2023 +0500
    
        Add test editing
    
    commit 206438881cd12ea0b96e8c44c165793a155ded6b
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 19:07:40 2023 +0500
    
        Add "create test" button
    
    commit b421b920283cbb09ef23e796357927a3e4dc5c19
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 16:22:53 2023 +0500
    
        Use options as text_answers
    
    commit a47f982dc5ff5f015688fb67f9cc0ae3b1d32a00
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 15:48:22 2023 +0500
    
        Remove `TextAnswer`
    
    commit 53741abbefdcec5caabd8bcdc86d2514d1d5ff50
    Author: Eugene <git@telegrafo.ru>
    Date:   Sun Nov 19 15:42:39 2023 +0500
    
        Add test form

--- lib/testiroom_web/live/test/test_form_live.ex
+++ lib/testiroom_web/live/test/test_form_live.ex
@@ -34,6 +34,7 @@ defmodule TestiroomWeb.TestFormLive do
     end
   end
 
+  @impl true
   def handle_event("validate", %{"test" => test_params}, socket) do
     changeset =
       socket.assigns.test
@@ -43,60 +44,14 @@ defmodule TestiroomWeb.TestFormLive do
     {:noreply, assign_form(socket, changeset)}
   end
 
-  @impl true
-  def handle_event("create", %{"add-task" => _value, "test" => test_params}, socket) do
-    test_params =
-      Map.update(test_params, "tasks", %{"0" => %{}}, fn tasks ->
-        id =
-          tasks
-          |> Enum.max_by(fn {id, _task} ->
-            String.to_integer(id)
-          end)
-          |> elem(0)
-          |> String.to_integer()
-          |> Kernel.+(1)
-          |> to_string()
-
-        Map.put(tasks, id, %{})
-      end)
-
-    changeset =
-      socket.assigns.test
-      |> Exams.change_test(test_params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, assign_form(socket, changeset)}
-  end
-
-  @impl true
-  def handle_event("create", %{"add-option" => task_index, "test" => test_params}, socket) do
-    test_params =
-      Map.update!(
-        test_params,
-        "tasks",
-        &Map.update!(
-          &1,
-          task_index,
-          fn task ->
-            Map.update(task, "options", %{"0" => %{}}, fn options ->
-              id = fetch_next_id(options)
-
-              Map.put(options, id, %{})
-            end)
-          end
-        )
-      )
-
-    changeset =
-      socket.assigns.test
-      |> Exams.change_test(test_params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, assign_form(socket, changeset)}
-  end
-
   def handle_event("create", %{"test" => test_params}, socket) do
-    case Exams.insert_test(socket.assigns.test, test_params) do
+    insert_test(socket.assigns.live_action, test_params, socket)
+  end
+
+  defp insert_test(action, params, socket)
+
+  defp insert_test(:new, params, socket) do
+    case Exams.create_test(socket.assigns.test, params) do
       {:ok, test} ->
         socket =
           socket
@@ -110,42 +65,25 @@ defmodule TestiroomWeb.TestFormLive do
     end
   end
 
-  @impl true
-  def handle_info({TaskForm, task_changeset, index}, socket) do
-    socket =
-      update(socket, :form, fn %{source: changeset} ->
-        original_tasks = Ecto.Changeset.get_change(changeset, :tasks)
+  defp insert_test(:edit, params, socket) do
+    case Exams.update_test(socket.assigns.test, params) do
+      {:ok, test} ->
+        socket =
+          socket
+          |> put_flash(:info, "Тест успешно обновлён")
+          |> push_navigate(to: ~p"/tests/#{test}/exam")
 
-        changed_tasks =
-          if not Ecto.Changeset.get_change(task_changeset, :delete, false) ||
-               task_changeset.data.id do
-            List.update_at(original_tasks, index, fn _changeset -> task_changeset end)
-          else
-            List.delete_at(original_tasks, index)
-          end
+        {:noreply, socket}
 
-        changeset = Ecto.Changeset.put_change(changeset, :tasks, changed_tasks)
-        to_form(changeset)
-      end)
-
-    {:noreply, socket}
+      {:error, changeset} ->
+        {:noreply, assign_form(socket, changeset)}
+    end
   end
 
   defp assign_form(socket, changeset) do
     assign(socket,
       form: to_form(changeset)
     )
-  end
-
-  defp fetch_next_id(map) do
-    map
-    |> Enum.max_by(fn {id, _value} ->
-      String.to_integer(id)
-    end)
-    |> elem(0)
-    |> String.to_integer()
-    |> Kernel.+(1)
-    |> to_string()
   end
 
   defp get_page_title(:new), do: "Создание теста"
