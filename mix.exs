048d567bee2e603388b52bdf5b99c14df6d73cc1
Eugene <git@telegrafo.ru>
2023-11-26 20:35:51 +0500

    Revert "Remove erlang libs from applications"
    This reverts commit 9502a6fea443f5f7e6378e10fb410f342b31b984.

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Testiroom.MixProject do
   def application do
     [
       mod: {Testiroom.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :wx, :observer]
     ]
   end
 
