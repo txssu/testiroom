60a6a76c91c70709702c9125956001168ecc89d5
Eugene <git@telegrafo.ru>
2023-12-11 16:17:36 +0500

    Add grades validation

--- lib/testiroom/exams/test.ex
+++ lib/testiroom/exams/test.ex
@@ -2,6 +2,7 @@ defmodule Testiroom.Exams.Test do
   use Ecto.Schema
 
   import Ecto.Changeset
+  import TestiroomWeb.Gettext
 
   alias Testiroom.Accounts.User
   alias Testiroom.Exams.Grade
@@ -23,7 +24,7 @@ defmodule Testiroom.Exams.Test do
 
     belongs_to :user, User
 
-    has_many :grades, Grade, on_replace: :delete
+    has_many :grades, Grade, on_replace: :delete, preload_order: [desc: :from]
     has_many :tasks, Task, on_replace: :delete
 
     timestamps(type: :utc_datetime)
@@ -49,11 +50,22 @@ defmodule Testiroom.Exams.Test do
       drop_param: :grades_delete
     )
     |> convert_duration_to_seconds()
+    |> validate_grades()
   end
 
   def convert_duration_to_seconds(changeset) do
     minutes = get_change(changeset, :duration_in_minutes)
 
     put_change(changeset, :duration_in_seconds, minutes && minutes * 60)
+  end
+
+  def validate_grades(changeset) do
+    grades = get_field(changeset, :grades) |> IO.inspect()
+
+    if grades == [] or Enum.find(grades, &(&1.from == 0)) do
+      changeset
+    else
+      add_error(changeset, :grades, gettext("should cover values from 0 to 100 (you need to add a grade from 0%)"))
+    end
   end
 end
