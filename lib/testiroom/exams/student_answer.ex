a0bb90a5de51cae01d13707f9e3628e977921fc6
Eugene <git@telegrafo.ru>
2023-12-17 14:54:31 +0500

    Add `correct?` to `StudentAnswer`

--- lib/testiroom/exams/student_answer.ex
+++ lib/testiroom/exams/student_answer.ex
@@ -28,4 +28,43 @@ defmodule Testiroom.Exams.StudentAnswer do
     |> cast(attrs, [:text_input])
     |> validate_required([])
   end
+
+  def get_score(student_answer) do
+    max = student_answer.task.score
+
+    if correct?(student_answer) do
+      {max, max}
+    else
+      {0, max}
+    end
+  end
+
+  def correct?(student_answer) do
+    task = student_answer.task
+
+    case task.type do
+      :text ->
+        answers = Enum.map(task.options, &Map.fetch!(&1, :text))
+        student_answer.text_input in answers
+
+      :single ->
+        [selected_option] = student_answer.selected_options
+        selected_option.is_correct
+
+      :multiple ->
+        selected_options = student_answer.selected_options
+        task_options = student_answer.task.options
+
+        all_selected_correct?(selected_options) and
+          exact_correct_count?(selected_options, task_options)
+    end
+  end
+
+  defp all_selected_correct?(selected_options) do
+    Enum.all?(selected_options, & &1.is_correct)
+  end
+
+  defp exact_correct_count?(selected_options, task_options) do
+    Enum.count(selected_options) == Enum.count(task_options, & &1.is_correct)
+  end
 end
