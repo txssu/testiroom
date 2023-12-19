8e62cb23b41347933a70ba073d6598f9de4c671b
Eugene <git@telegrafo.ru>
2023-12-19 22:08:01 +0500

    Disable mail in prod

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ config :testiroom, TestiroomWeb.Endpoint, cache_static_manifest: "priv/static/ca
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Testiroom.Finch
 
 # Disable Swoosh Local Memory Storage
-config :swoosh, local: false
+# config :swoosh, local: false
 
 # Do not print debug messages in production
 config :logger, level: :info
