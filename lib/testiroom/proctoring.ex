21d2d4af70e83274a2acda53eeea30f3433f42ff
Eugene <git@telegrafo.ru>
2023-12-25 21:18:49 +0500

    Update proctor registration to use a tuple key in Testiroom.Proctoring

--- lib/testiroom/proctoring.ex
+++ lib/testiroom/proctoring.ex
@@ -1,11 +1,11 @@
 defmodule Testiroom.Proctoring do
   @moduledoc false
   def register_proctor(test_id) do
-    Registry.register(__MODULE__, test_id, [])
+    Registry.register(__MODULE__, {:proctor, test_id}, [])
   end
 
   def notify_proctor(test_id, event) do
-    Registry.dispatch(__MODULE__, test_id, fn entries ->
+    Registry.dispatch(__MODULE__, {:proctor, test_id}, fn entries ->
       for {pid, _value} <- entries, do: send(pid, event)
     end)
   end
