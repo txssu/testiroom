53d50da13aab7af3c0d27a161419043ab1f52654
Eugene <git@telegrafo.ru>
2023-12-25 21:12:57 +0500

    Restrict test editor access to the test creator only

--- lib/testiroom_web/live/test_live/edit.ex
+++ lib/testiroom_web/live/test_live/edit.ex
@@ -8,9 +8,18 @@ defmodule TestiroomWeb.TestLive.Edit do
 
   @impl Phoenix.LiveView
   def mount(%{"test_id" => id}, _session, socket) do
-    {:ok,
-     socket
-     |> assign(:page_title, gettext("Edit Test"))
-     |> assign(:test, Exams.get_test!(id))}
+    test = Exams.get_test!(id)
+
+    if test.user_id == socket.assigns.current_user.id do
+      {:ok,
+       socket
+       |> assign(:page_title, gettext("Edit Test"))
+       |> assign(:test, test)}
+    else
+      {:ok,
+       socket
+       |> put_flash(:error, gettext("You cannot edit other users' tests."))
+       |> push_navigate(to: ~p"/tests")}
+    end
   end
 end
