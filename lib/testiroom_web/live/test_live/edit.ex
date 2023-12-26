9e98e7f93414124cf68d1412e657d52b9049e2a5
Eugene <git@telegrafo.ru>
2023-12-26 14:31:35 +0500

    Revert "Remove `handle_params` from `TestLive.Edit`"
    This reverts commit 6213ad5a2438a3e521fb1c8573e1b6542dee1467.

--- lib/testiroom_web/live/test_live/edit.ex
+++ lib/testiroom_web/live/test_live/edit.ex
@@ -7,16 +7,21 @@ defmodule TestiroomWeb.TestLive.Edit do
   alias Testiroom.Exams
 
   @impl Phoenix.LiveView
-  def mount(%{"test_id" => id}, _session, socket) do
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_params(%{"test_id" => id}, _uri, socket) do
     test = Exams.get_test!(id)
 
     if test.user_id == socket.assigns.current_user.id do
-      {:ok,
+      {:noreply,
        socket
        |> assign(:page_title, gettext("Edit Test"))
        |> assign(:test, test)}
     else
-      {:ok,
+      {:noreply,
        socket
        |> put_flash(:error, gettext("You cannot edit other users' tests."))
        |> push_navigate(to: ~p"/tests")}
