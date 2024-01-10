c64dacd0f42eb5799caf5d9917cf64d467aa7436
Eugene <git@telegrafo.ru>
2024-01-10 16:30:07 +0500

    feat(proctoring): add time per task display

--- lib/testiroom_web/live/proctor_live/progress.ex
+++ lib/testiroom_web/live/proctor_live/progress.ex
@@ -18,12 +18,41 @@ defmodule TestiroomWeb.ProctorLive.Progress do
      socket
      |> assign(:test, test)
      |> assign(:monitor, %Monitor{})
-     |> assign(:max_task_order, max_task_order)}
+     |> assign(:max_task_order, max_task_order)
+     |> assign(:tasks_count, max_task_order + 1)}
   end
 
   @impl Phoenix.LiveView
   def handle_info({:proctoring, event}, socket) do
     {:noreply, update(socket, :monitor, &Monitor.handle(&1, event))}
+  end
+
+  def format_time_per_task(%Monitor{spended_time_per_task: spended_time, started_counter: started}, tasks_count) do
+    milliseconds =
+      if started == 0 do
+        0
+      else
+        spended_time
+        |> Map.values()
+        |> Enum.sum()
+        |> div(tasks_count * started)
+      end
+
+    format_millisecodns(milliseconds)
+  end
+
+  defp format_millisecodns(milliseconds) do
+    total_seconds = div(milliseconds, 1000)
+    seconds = total_seconds |> rem(60) |> add_leading_zero()
+    minutes = total_seconds |> div(60) |> add_leading_zero()
+
+    "#{minutes}:#{seconds}"
+  end
+
+  defp add_leading_zero(time) do
+    time
+    |> Integer.to_string()
+    |> String.pad_leading(2, "0")
   end
 
   slot :inner_block, required: true
